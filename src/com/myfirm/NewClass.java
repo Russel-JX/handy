286c476acb3af6c4c67e7c45ec2198ef8f501193
jiangxun <jxoucjj@126.com>
2021-12-07 22:05:53 +0800

    update NewClass file

--- src/com/myfirm/NewClass.java
+++ src/com/myfirm/NewClass.java
@@ -1,26 +1,9 @@
 package com.myfirm;
 
-public class Main {
+public class NewClass {
 
-    private String name;
-    private boolean gender;
+    private String school;
+    private boolean address;
 
-    public static void main(String[] args) {
-        Product product = new Product("keyboard");
-        System.out.println("Hi, world! "+" product name is: "+product.getName());
-    }
 
-    @Override
-    public String toString() {
-        String newName = this.name;
-        return super.toString();
-    }
-
-    public boolean isEmpty() {
-        return name.isEmpty();
-    }
-
-    public void getF() {
-
-    }
 }
